e582ce39ca9f474dffb3ebba44bf68bf9b7151e8
smashasmash <sasha.moroz@gmail.com>
2026-06-05 12:59:42 -0700

    Wrap App with password protection

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { PasswordProtect } from './PasswordProtect.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PasswordProtect>
+      <App />
+    </PasswordProtect>
   </StrictMode>,
 )
